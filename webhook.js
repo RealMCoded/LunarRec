c542a6c1cde461d4821c6e89553a159b40b01e9d
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-02-23 13:47:08 -0500

    webhook object

--- webhook.js
+++ webhook.js
@@ -2,7 +2,18 @@ const fetch = require("node-fetch")
 const { webhook } = require("./config.json")
 
 async function sendWebhook(data){
-    if (webhook == null || webhook == "") return;
+    if (webhook.console == null || webhook.console == "") return;
+    await fetch(webhook,{
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({content: data})
+    });
+}
+
+async function sendReport(data){
+    if (webhook.reports == null || webhook.reports == "") return;
     await fetch(webhook,{
         method: 'post',
         headers: {
